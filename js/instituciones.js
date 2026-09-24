14df132a0d8c8af995774eb2ef10546de2fbf0f7
pausaactivaepc-collab <pausaactiva.epc@gmail.com>
2026-09-24 17:50:41 -0300

    Refactor cargarInstituciones and mostrarInstituciones

--- js/instituciones.js
+++ js/instituciones.js
@@ -192,27 +192,7 @@ function mostrarInstituciones(instituciones) {
                 :
                 ""
             }
-                  ${
-                        actividad.sitioWeb &&
-                        actividad.sitioWeb !== "..."
-                        ?
-                        `
-                        <a
-                            href="${actividad.sitioWeb}"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            class="btn-red-social">
-
-                            <i data-lucide="globe"></i>
-
-                            Sitio web
-
-                        </a>
-                        `
-                        :
-                        ""
-                    }
-
+                
         </div>
 
     </div>
